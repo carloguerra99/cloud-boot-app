f9739dedeca9209c47176ab8b68b52d758a3922c
carloguerra99 <carlo.guerra99@icloud.com>
2023-07-06 12:36:00 +0200

    init repo

--- Home.py
+++ Home.py
@@ -3,3 +3,13 @@ import streamlit as st
 st.title("Hello everyone!")
 
 st.header("This is a Streamlit header!")
+
+
+selected_option = st.selectbox(
+    "Seleziona opzione:",
+    ["Opzione 1", "Opzione 2"]
+)
+st.write(selected_option)
+
+
+st.image("https://www.ldlc.com/it-it/scheda/PB00395869.html")
